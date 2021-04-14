5605435a95f90d637488bb9bd24d61494ba2386d
John Madden <jck13mad@gmail.com>
2021-04-13 21:37:09 -0500

    create comment static

--- src/comment.js
+++ src/comment.js
@@ -5,5 +5,16 @@ class Comment {
         this.menu_item_id = comment.menu_item_id
     }
 
-    
+    static createComment(e){
+        e.preventDefault()
+        const li = document.createElement('li')
+        const commentContent = e.target.children[0].value
+        const commentList = e.target.previousElementSibling
+        const menuItemID = e.target.parentElement.dataset.id
+
+        Comment.submitComment(commentContent, commentList, menuItemID, li)
+        e.target.reset()
+    }
+
+
 }
