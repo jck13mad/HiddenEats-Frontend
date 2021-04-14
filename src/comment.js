f25571cdfcde3e6ffa9dcff0aa7ce396caf6d789
John Madden <jck13mad@gmail.com>
2021-04-13 21:30:20 -0500

    Added constructor for comments

---
+++ src/comment.js
@@ -0,0 +1,9 @@
+class Comment {
+    constructor(comment){
+        this.id = comment.id 
+        this.content = comment.content 
+        this.menu_item_id = comment.menu_item_id
+    }
+
+    
+}
